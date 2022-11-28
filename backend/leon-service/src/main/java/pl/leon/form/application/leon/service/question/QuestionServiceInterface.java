68c8630017dfe0b813f6d9af60b95175ce716c41
shane3102 <bkucharczyk2000@gmail.com>
2022-11-28 20:34:28 +0100

    add generic interface for getting random question from database

---
+++ backend/leon-service/src/main/java/pl/leon/form/application/leon/service/question/QuestionServiceInterface.java
@@ -0,0 +1,48 @@
+package pl.leon.form.application.leon.service.question;
+
+import org.hibernate.Hibernate;
+import org.hibernate.proxy.HibernateProxy;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+import pl.leon.form.application.leon.mapper.manager.QuestionMapperManager;
+import pl.leon.form.application.leon.model.response.questions.QuestionResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public interface QuestionServiceInterface<T> {
+
+    JpaRepository<T, Long> getRepository();
+
+    QuestionMapperManager getQuestionMapperManager();
+
+    default List<QuestionResponse> getRandomQuestions(Short count) {
+        long allQuestions = getRepository().count();
+
+        if (allQuestions < count) {
+            // TODO custom exception
+            throw new RuntimeException();
+        }
+
+        List<Long> randomQuestionIds = new ArrayList<>();
+        while (randomQuestionIds.size() < count) {
+            Long randIndex = Double.valueOf(Math.ceil(Math.random() * count)).longValue();
+            if (!randomQuestionIds.contains(randIndex)) {
+                randomQuestionIds.add(randIndex);
+            }
+        }
+
+        return randomQuestionIds.stream()
+                .map(getRepository()::getById)
+                .map(entity -> {
+                    if (entity instanceof HibernateProxy) {
+                        return (T) ((HibernateProxy) entity).getHibernateLazyInitializer().getImplementation();
+                    }
+                    return entity;
+                })
+                .map(getQuestionMapperManager()::mapToResponse)
+                .collect(Collectors.toList());
+    }
+}
