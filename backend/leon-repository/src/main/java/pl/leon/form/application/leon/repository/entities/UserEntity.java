8c1039b637717934d4a850ee293dda24a7556891
shane3102 <bkucharczyk2000@gmail.com>
2022-12-03 21:10:25 +0100

    add security dependencies, add global dependency for jackson databind (every module can use ObjectMapper now c:)

---
+++ backend/leon-repository/src/main/java/pl/leon/form/application/leon/repository/entities/UserEntity.java
@@ -0,0 +1,68 @@
+package pl.leon.form.application.leon.repository.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.Fetch;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.CascadeType;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "USERS")
+public class UserEntity implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    //TODO na enum
+    private boolean isEnabled;
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roles;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.MERGE)
+    private List<FormEntity> addedForms;
+
+    // TODO zaimplementować metody poniżej (może coś się przyda)
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+}
