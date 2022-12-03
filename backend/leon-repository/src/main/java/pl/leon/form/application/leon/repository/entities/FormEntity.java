d263cba260535ba9bd2cb0cd9063e43c595f9683
shane3102 <bkucharczyk2000@gmail.com>
2022-12-03 21:09:01 +0100

    add simple user entity, add pointing form to user, remove web security config

--- backend/leon-repository/src/main/java/pl/leon/form/application/leon/repository/entities/FormEntity.java
+++ backend/leon-repository/src/main/java/pl/leon/form/application/leon/repository/entities/FormEntity.java
@@ -13,15 +13,23 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.util.List;
 
 @Data
 @Entity
+@Table(name = "form")
 public class FormEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
 
     private String subject;
 
