acd4ce873d8b510bbcb42abbaf88a237462deb4d
shane3102 <bkucharczyk2000@gmail.com>
2022-12-12 21:18:18 +0100

    add test for listing forms

--- backend/leon-web/src/test/java/pl/leon/form/application/leon/web/controller/form/FormSimpleCrudTest.java
+++ backend/leon-web/src/test/java/pl/leon/form/application/leon/web/controller/form/FormSimpleCrudTest.java
@@ -1,6 +1,7 @@
 package pl.leon.form.application.leon.web.controller.form;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Nested;
@@ -17,13 +18,16 @@ import pl.leon.form.application.leon.model.request.FormRequest;
 import pl.leon.form.application.leon.model.request.questions.OptionRequest;
 import pl.leon.form.application.leon.model.request.questions.QuestionRequest;
 import pl.leon.form.application.leon.model.response.FormResponse;
+import pl.leon.form.application.leon.model.response.FormSnippetResponse;
 import pl.leon.form.application.leon.repository.DropdownQuestionRepository;
+import pl.leon.form.application.leon.repository.FormRepository;
 import pl.leon.form.application.leon.repository.LineScaleQuestionRepository;
 import pl.leon.form.application.leon.repository.LongAnswerQuestionRepository;
 import pl.leon.form.application.leon.repository.MultipleChoiceQuestionRepository;
 import pl.leon.form.application.leon.repository.ShortAnswerQuestionRepository;
 import pl.leon.form.application.leon.repository.SingleChoiceQuestionRepository;
 import pl.leon.form.application.leon.repository.UserRepository;
+import pl.leon.form.application.leon.repository.entities.FormEntity;
 import pl.leon.form.application.leon.repository.entities.UserEntity;
 
 import java.time.LocalDate;
@@ -34,6 +38,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -51,45 +56,7 @@ public class FormSimpleCrudTest {
 
     private static final String URL = "/api/form";
 
-    private static final LocalDate FORM_DATE_TO = LocalDate.now().plusMonths(1);
-    private static final boolean DISABLE_FORM_QUESTIONS_AFTER_DATE_TO = true;
-
-    private static final String QUESTION_1_CONTENT_DROPDOWN = "Pytanie 1 lista wybieralna";
-    private static final String QUESTION_2_CONTENT_DROPDOWN = "Pytanie 2 lista wybieralna";
-    private static final String QUESTION_3_CONTENT_DROPDOWN = "Pytanie 3 lista wybieralna";
-    private static final String QUESTION_1_CONTENT_LINE_SCALE = "Pytanie 1 skala liniowa";
-    private static final String QUESTION_2_CONTENT_LINE_SCALE = "Pytanie 2 skala liniowa";
-    private static final String QUESTION_1_CONTENT_LONG_ANSWER = "Pytanie 1 długa odpowiedz";
-    private static final String QUESTION_2_CONTENT_LONG_ANSWER = "Pytanie 2 długa odpowiedz";
-    private static final String QUESTION_3_CONTENT_LONG_ANSWER = "Pytanie 3 długa odpowiedz";
-    private static final String QUESTION_4_CONTENT_LONG_ANSWER = "Pytanie 4 długa odpowiedz";
-    private static final String QUESTION_1_CONTENT_MULTIPLE_CHOICE = "Pytanie 1 wielokrotnego wyboru";
-    private static final String QUESTION_2_CONTENT_MULTIPLE_CHOICE = "Pytanie 2 wielokrotnego wyboru";
-    private static final String QUESTION_1_CONTENT_SHORT_ANSWER = "Pytanie 1 krótka odpowiedz";
-    private static final String QUESTION_1_CONTENT_SINGLE_CHOICE = "Pytanie 1 jednokrotnego wyboru";
-    private static final String QUESTION_2_CONTENT_SINGLE_CHOICE = "Pytanie 2 jednokrotnego wyboru";
-
-    private static final List<String> DROPDOWN_QUESTION_CONTENTS = List.of(QUESTION_1_CONTENT_DROPDOWN, QUESTION_2_CONTENT_DROPDOWN, QUESTION_3_CONTENT_DROPDOWN);
-    private static final List<String> LINE_SCALE_QUESTION_CONTENTS = List.of(QUESTION_1_CONTENT_LINE_SCALE, QUESTION_2_CONTENT_LINE_SCALE);
-    private static final List<String> LONG_ANSWER_QUESTION_CONTENTS = List.of(QUESTION_1_CONTENT_LONG_ANSWER, QUESTION_2_CONTENT_LONG_ANSWER, QUESTION_3_CONTENT_LONG_ANSWER, QUESTION_4_CONTENT_LONG_ANSWER);
-    private static final List<String> MULTIPLE_CHOICE_QUESTION_CONTENTS = List.of(QUESTION_1_CONTENT_MULTIPLE_CHOICE, QUESTION_2_CONTENT_MULTIPLE_CHOICE);
-    private static final List<String> SHORT_ANSWER_QUESTION_CONTENTS = List.of(QUESTION_1_CONTENT_SHORT_ANSWER);
-    private static final List<String> SINGLE_CHOICE_QUESTION_CONTENTS = List.of(QUESTION_1_CONTENT_SINGLE_CHOICE, QUESTION_2_CONTENT_SINGLE_CHOICE);
-
     private static final String USERNAME = "user";
-
-    private static final String ANSWER_1 = "Odpowiedz 1";
-    private static final String ANSWER_2 = "Odpowiedz 2";
-    private static final String ANSWER_3 = "Odpowiedz 3";
-    private static final String ANSWER_4 = "Odpowiedz 4";
-
-    private static final Long EXPECTED_DROPDOWN_QUESTIONS = 3L;
-    private static final Long EXPECTED_LINE_SCALE_QUESTIONS = 2L;
-    private static final Long EXPECTED_LONG_ANSWER_QUESTIONS = 4L;
-    private static final Long EXPECTED_MULTIPLE_CHOICE_QUESTIONS = 2L;
-    private static final Long EXPECTED_SHORT_ANSWER_QUESTIONS = 1L;
-    private static final Long EXPECTED_SINGLE_CHOICE_QUESTIONS = 2L;
-
 
     @Autowired
     private MockMvc mockMvc;
@@ -116,11 +83,51 @@ public class FormSimpleCrudTest {
     private SingleChoiceQuestionRepository singleChoiceQuestionRepository;
 
     @Autowired
+    private FormRepository formRepository;
+
+    @Autowired
     private ObjectMapper mapper;
 
     @Nested
     @TestInstance(TestInstance.Lifecycle.PER_CLASS)
     class AddNewFormTest {
+
+        private final String ANSWER_1 = "Odpowiedz 1";
+        private final String ANSWER_2 = "Odpowiedz 2";
+        private final String ANSWER_3 = "Odpowiedz 3";
+        private final String ANSWER_4 = "Odpowiedz 4";
+
+        private final Long EXPECTED_DROPDOWN_QUESTIONS = 3L;
+        private final Long EXPECTED_LINE_SCALE_QUESTIONS = 2L;
+        private final Long EXPECTED_LONG_ANSWER_QUESTIONS = 4L;
+        private final Long EXPECTED_MULTIPLE_CHOICE_QUESTIONS = 2L;
+        private final Long EXPECTED_SHORT_ANSWER_QUESTIONS = 1L;
+        private final Long EXPECTED_SINGLE_CHOICE_QUESTIONS = 2L;
+
+        private final LocalDate FORM_DATE_TO = LocalDate.now().plusMonths(1);
+        private final boolean DISABLE_FORM_QUESTIONS_AFTER_DATE_TO = true;
+
+        private final String QUESTION_1_CONTENT_DROPDOWN = "Pytanie 1 lista wybieralna";
+        private final String QUESTION_2_CONTENT_DROPDOWN = "Pytanie 2 lista wybieralna";
+        private final String QUESTION_3_CONTENT_DROPDOWN = "Pytanie 3 lista wybieralna";
+        private final String QUESTION_1_CONTENT_LINE_SCALE = "Pytanie 1 skala liniowa";
+        private final String QUESTION_2_CONTENT_LINE_SCALE = "Pytanie 2 skala liniowa";
+        private final String QUESTION_1_CONTENT_LONG_ANSWER = "Pytanie 1 długa odpowiedz";
+        private final String QUESTION_2_CONTENT_LONG_ANSWER = "Pytanie 2 długa odpowiedz";
+        private final String QUESTION_3_CONTENT_LONG_ANSWER = "Pytanie 3 długa odpowiedz";
+        private final String QUESTION_4_CONTENT_LONG_ANSWER = "Pytanie 4 długa odpowiedz";
+        private final String QUESTION_1_CONTENT_MULTIPLE_CHOICE = "Pytanie 1 wielokrotnego wyboru";
+        private final String QUESTION_2_CONTENT_MULTIPLE_CHOICE = "Pytanie 2 wielokrotnego wyboru";
+        private final String QUESTION_1_CONTENT_SHORT_ANSWER = "Pytanie 1 krótka odpowiedz";
+        private final String QUESTION_1_CONTENT_SINGLE_CHOICE = "Pytanie 1 jednokrotnego wyboru";
+        private final String QUESTION_2_CONTENT_SINGLE_CHOICE = "Pytanie 2 jednokrotnego wyboru";
+
+        private final List<String> DROPDOWN_QUESTION_CONTENTS = List.of(QUESTION_1_CONTENT_DROPDOWN, QUESTION_2_CONTENT_DROPDOWN, QUESTION_3_CONTENT_DROPDOWN);
+        private final List<String> LINE_SCALE_QUESTION_CONTENTS = List.of(QUESTION_1_CONTENT_LINE_SCALE, QUESTION_2_CONTENT_LINE_SCALE);
+        private final List<String> LONG_ANSWER_QUESTION_CONTENTS = List.of(QUESTION_1_CONTENT_LONG_ANSWER, QUESTION_2_CONTENT_LONG_ANSWER, QUESTION_3_CONTENT_LONG_ANSWER, QUESTION_4_CONTENT_LONG_ANSWER);
+        private final List<String> MULTIPLE_CHOICE_QUESTION_CONTENTS = List.of(QUESTION_1_CONTENT_MULTIPLE_CHOICE, QUESTION_2_CONTENT_MULTIPLE_CHOICE);
+        private final List<String> SHORT_ANSWER_QUESTION_CONTENTS = List.of(QUESTION_1_CONTENT_SHORT_ANSWER);
+        private final List<String> SINGLE_CHOICE_QUESTION_CONTENTS = List.of(QUESTION_1_CONTENT_SINGLE_CHOICE, QUESTION_2_CONTENT_SINGLE_CHOICE);
 
         private FormRequest formRequest;
 
@@ -306,5 +313,59 @@ public class FormSimpleCrudTest {
             );
         }
 
+    }
+
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    class ListFormsTest {
+
+        private final Long EXPECTED_FORM_LIST_LENGTH = 4L;
+
+        private final String FORM_1_TOPIC = "Formularz temat 1";
+        private final String FORM_2_TOPIC = "Formularz temat 2";
+        private final String FORM_3_TOPIC = "Formularz temat 3";
+        private final String FORM_4_TOPIC = "Formularz temat 4";
+
+        private final List<String> FORM_TOPICS = List.of(FORM_1_TOPIC, FORM_2_TOPIC, FORM_3_TOPIC, FORM_4_TOPIC);
+
+        private final String USERNAME_1 = "user1";
+        private final String USERNAME_2 = "user2";
+        private final String USERNAME_3 = "user3";
+
+        @BeforeAll
+        void beforeAll() {
+            UserEntity user1 = userRepository.save(UserEntity.builder().username(USERNAME_1).build());
+            UserEntity user2 = userRepository.save(UserEntity.builder().username(USERNAME_2).build());
+            UserEntity user3 = userRepository.save(UserEntity.builder().username(USERNAME_3).build());
+
+            formRepository.save(FormEntity.builder().user(user1).subject(FORM_1_TOPIC).build());
+            formRepository.save(FormEntity.builder().user(user2).subject(FORM_2_TOPIC).build());
+            formRepository.save(FormEntity.builder().user(user3).subject(FORM_3_TOPIC).build());
+            formRepository.save(FormEntity.builder().user(user1).subject(FORM_4_TOPIC).build());
+
+        }
+
+        @Test
+        void givenPersistedForms_whenListForms_thenAllPersistedFormsListed() throws Exception {
+            // given
+
+            // when
+            String jsonResponse = mockMvc.perform(get(URL + "/list"))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andReturn()
+                    .getResponse()
+                    .getContentAsString();
+
+            List<FormSnippetResponse> responseList = mapper.readValue(jsonResponse, new TypeReference<>() {
+            });
+
+            // then
+            assertAll(
+                    () -> assertNotNull(responseList),
+                    () -> assertEquals(EXPECTED_FORM_LIST_LENGTH, responseList.size()),
+                    () -> assertTrue(responseList.stream().allMatch(form -> FORM_TOPICS.contains(form.getSubject())))
+            );
+        }
     }
 }
