d7926c2f3f726c23fe6600a270aa2857165537fb
shane3102 <bkucharczyk2000@gmail.com>
2022-12-15 21:20:24 +0100

    another thing I forgot xd

--- backend/leon-service/src/main/java/pl/leon/form/application/leon/model/both/questions/QuestionAnswering.java
+++ backend/leon-service/src/main/java/pl/leon/form/application/leon/model/both/questions/QuestionAnswering.java
@@ -1,18 +1,19 @@
-package pl.leon.form.application.leon.model.request.questions;
+package pl.leon.form.application.leon.model.both.questions;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import pl.leon.form.application.leon.model.both.Option;
 import pl.leon.form.application.leon.model.both.questions.type.QuestionType;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class QuestionAnsweringRequest {
+public class QuestionAnswering {
     private Long id;
-    private OptionRequest chosenOption;
+    private Option chosenOption;
     private String answer;
     private QuestionType type;
 }
