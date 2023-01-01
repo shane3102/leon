574d4956acb34776db48cec195c7c051daaa6e49
shane3102 <bkucharczyk2000@gmail.com>
2023-01-01 21:11:53 +0100

    mark repositories as... repositories

--- backend/leon-repository/src/main/java/pl/leon/form/application/leon/repository/FormCompletedRepository.java
+++ backend/leon-repository/src/main/java/pl/leon/form/application/leon/repository/FormCompletedRepository.java
@@ -1,7 +1,9 @@
 package pl.leon.form.application.leon.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.leon.form.application.leon.repository.entities.FormCompletedEntity;
 
+@Repository
 public interface FormCompletedRepository extends JpaRepository<FormCompletedEntity, Long> {
 }
