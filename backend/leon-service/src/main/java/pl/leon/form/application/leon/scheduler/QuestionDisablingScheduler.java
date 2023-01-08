0a739c0f89649d9041fa9767fef4eb9e965f4827
shane3102 <bkucharczyk2000@gmail.com>
2023-01-08 18:25:07 +0100

    use written method, add disabling forms after date to

--- backend/leon-service/src/main/java/pl/leon/form/application/leon/scheduler/QuestionDisablingScheduler.java
+++ backend/leon-service/src/main/java/pl/leon/form/application/leon/scheduler/QuestionDisablingScheduler.java
@@ -2,75 +2,39 @@ package pl.leon.form.application.leon.scheduler;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import pl.leon.form.application.leon.repository.DropdownQuestionRepository;
 import pl.leon.form.application.leon.repository.FormRepository;
-import pl.leon.form.application.leon.repository.LineScaleQuestionRepository;
-import pl.leon.form.application.leon.repository.LongAnswerQuestionRepository;
-import pl.leon.form.application.leon.repository.MultipleChoiceQuestionRepository;
-import pl.leon.form.application.leon.repository.ShortAnswerQuestionRepository;
-import pl.leon.form.application.leon.repository.SingleChoiceQuestionRepository;
 import pl.leon.form.application.leon.repository.entities.FormEntity;
+import pl.leon.form.application.leon.service.question.interfaces.QuestionServiceInterface;
 
-import java.time.LocalDate;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Slf4j
 @Service
 @AllArgsConstructor
+@SuppressWarnings("unchecked")
 public class QuestionDisablingScheduler {
 
     private final FormRepository formRepository;
 
-    private final DropdownQuestionRepository dropdownQuestionRepository;
-    private final LineScaleQuestionRepository lineScaleQuestionRepository;
-    private final LongAnswerQuestionRepository longAnswerQuestionRepository;
-    private final MultipleChoiceQuestionRepository multipleChoiceQuestionRepository;
-    private final ShortAnswerQuestionRepository shortAnswerQuestionRepository;
-    private final SingleChoiceQuestionRepository singleChoiceQuestionRepository;
+    @Autowired
+    private final List<QuestionServiceInterface> questionServices;
 
     @Scheduled(cron = "0 0 0 * * *")
-    private void disableQuestions() {
-        log.info("Disabling expired questions...");
-        List<FormEntity> forms = formRepository.findAll();
+    public void disableExpiredQuestionsAndForms() {
+        log.info("Disabling expired questions and forms...");
 
-        forms.forEach(form -> {
-            if (LocalDate.now().isEqual(form.getDateTo()) && form.isDisableQuestionsAfterDateTo()) {
-                dropdownQuestionRepository.saveAll(
-                        form.getDropdownQuestions().stream()
-                                .peek(question -> question.setDisabled(true))
-                                .collect(Collectors.toList()));
+        Stream<FormEntity> formsEnding = (Stream<FormEntity>) questionServices.stream().flatMap(QuestionServiceInterface::disableQuestions);
 
-                lineScaleQuestionRepository.saveAll(
-                        form.getLineScaleQuestions().stream()
-                                .peek(question -> question.setDisabled(true))
-                                .collect(Collectors.toList()));
-
-                longAnswerQuestionRepository.saveAll(
-                        form.getLongAnswerQuestions().stream()
-                                .peek(question -> question.setDisabled(true))
-                                .collect(Collectors.toList()));
-
-                multipleChoiceQuestionRepository.saveAll(
-                        form.getMultipleChoiceQuestions().stream()
-                                .peek(question -> question.setDisabled(true))
-                                .collect(Collectors.toList()));
-
-                shortAnswerQuestionRepository.saveAll(
-                        form.getShortAnswerQuestions().stream()
-                                .peek(question -> question.setDisabled(true))
-                                .collect(Collectors.toList()));
-
-                singleChoiceQuestionRepository.saveAll(
-                        form.getSingleChoiceQuestions().stream()
-                                .peek(question -> question.setDisabled(true))
-                                .collect(Collectors.toList()));
-            }
-            log.info("Disabled questions from form: id: {}, subject: {}", form.getId(), form.getSubject());
+        formsEnding.forEach(form -> {
+            form.setDisabled(true);
+            formRepository.save(form);
         });
-        log.info("Disabling expired questions completed.");
+
+        log.info("Disabling expired questions and forms completed.");
     }
 
 }
