75ffe770dcb2ae6d80c402da1cc34f801ed7a42a
shane3102 <bkucharczyk2000@gmail.com>
2023-01-08 23:12:48 +0100

    changes in interfaces for persisting new answer and incrementing options

--- backend/leon-service/src/main/java/pl/leon/form/application/leon/service/question/interfaces/IncrementCountForOptionInterface.java
+++ backend/leon-service/src/main/java/pl/leon/form/application/leon/service/question/interfaces/IncrementCountForOptionInterface.java
@@ -3,5 +3,5 @@ package pl.leon.form.application.leon.service.question.interfaces;
 import pl.leon.form.application.leon.repository.entities.OptionEntity;
 
 public interface IncrementCountForOptionInterface<T> {
-    void incrementOption(T question, OptionEntity option);
+    void incrementOption(T question);
 }
