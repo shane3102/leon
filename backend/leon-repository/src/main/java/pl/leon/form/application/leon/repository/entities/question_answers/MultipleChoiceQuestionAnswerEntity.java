4f4b082dc8dd80f6b33695e704119e8faf56cd8d
shane3102 <bkucharczyk2000@gmail.com>
2023-01-08 23:09:29 +0100

    add new entities containing:
    answered question, what was the answer and duration required to answer it for each question type

---
+++ backend/leon-repository/src/main/java/pl/leon/form/application/leon/repository/entities/question_answers/MultipleChoiceQuestionAnswerEntity.java
@@ -0,0 +1,42 @@
+package pl.leon.form.application.leon.repository.entities.question_answers;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import pl.leon.form.application.leon.repository.entities.FormCompletedEntity;
+import pl.leon.form.application.leon.repository.entities.OptionEntity;
+import pl.leon.form.application.leon.repository.entities.questions.MultipleChoiceQuestionEntity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import java.time.Duration;
+import java.util.List;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MultipleChoiceQuestionAnswerEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.MERGE)
+    private MultipleChoiceQuestionEntity question;
+
+    @ManyToMany(cascade = CascadeType.MERGE)
+    private List<OptionEntity> options;
+
+    private Duration durationToAnswer;
+
+    @OneToOne(cascade = CascadeType.MERGE)
+    private FormCompletedEntity formCompleted;
+}
