0b880b67523f53692ae2808e5d0e013bcf6c27bd
shane3102 <bkucharczyk2000@gmail.com>
2023-01-15 17:02:18 +0100

    add new components to app module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
-import { ButtonComponent } from './components/headerButton/headerButton.component';
+import { ButtonComponent } from './components/header-button/header-button.component';
 import { MainPageButtonComponent } from './components/main-page-button/main-page-button.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { LoginStatusComponent } from './components/login-status/login-status.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +19,14 @@ import { FooterComponent } from './components/footer/footer.component';
     MainPageComponent,
     ButtonComponent,
     MainPageButtonComponent,
-    FooterComponent
+    FooterComponent,
+    LoginStatusComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
