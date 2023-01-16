1b5925fbf4ab1ca4e34098b0bd770e381547d217
shane3102 <bkucharczyk2000@gmail.com>
2023-01-16 17:19:50 +0100

    add frontend cors config, fix docker image

--- backend/leon-security/src/main/java/pl/leon/form/application/leon/security/JwtSecurityConfiguration.java
+++ backend/leon-security/src/main/java/pl/leon/form/application/leon/security/JwtSecurityConfiguration.java
@@ -12,6 +12,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import pl.leon.form.application.leon.core.exceptions.internal_server_error.concrete.ExceptionWhenFilterChainingAuthentication;
 import pl.leon.form.application.leon.service.UserService;
 
@@ -19,6 +21,9 @@ import pl.leon.form.application.leon.service.UserService;
 @RequiredArgsConstructor
 public class JwtSecurityConfiguration {
 
+
+    @Value("${front.url}")
+    private String FRONT_URL;
     @Value("${jwt.secret}")
     private String secret;
     @Autowired
@@ -60,5 +65,17 @@ public class JwtSecurityConfiguration {
         filter.setAuthenticationSuccessHandler(authenticationSuccessHandler);
         filter.setAuthenticationManager(authenticationManager);
         return filter;
+    }
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("*")
+                        .allowedOrigins(FRONT_URL);
+            }
+        };
     }
 }
