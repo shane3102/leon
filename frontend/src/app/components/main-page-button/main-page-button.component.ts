0793dd393febfd343d8c4495fc945db7a205de00
shane3102 <bkucharczyk2000@gmail.com>
2023-01-21 19:18:10 +0100

    implement main page buttons, add fontawesome

--- frontend/src/app/components/main-page-button/main-page-button.component.ts
+++ frontend/src/app/components/main-page-button/main-page-button.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { faCaretDown, faList, faListAlt, faListOl, faListUl } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-main-page-button',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-page-button.component.css']
 })
 export class MainPageButtonComponent implements OnInit {
+  @Input() text: string;
+  @Input() description: string;
+  @Input() iconClass: string;
+
+  downIcon = faCaretDown;
+  listIcon = faList;
+  listAltIcon = faListAlt;
+  listOlIcon = faListOl;
+  listUlIcon = faListUl;
 
   constructor() { }
 
