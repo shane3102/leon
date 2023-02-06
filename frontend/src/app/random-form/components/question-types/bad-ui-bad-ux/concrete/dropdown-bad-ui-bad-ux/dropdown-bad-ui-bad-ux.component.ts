ca797c64a88ef4342269e501ef636a47d4e5f167
shane3102 <bkucharczyk2000@gmail.com>
2023-02-06 16:43:05 +0100

    bind chose dropdown option to form control

--- frontend/src/app/random-form/components/question-types/bad-ui-bad-ux/concrete/dropdown-bad-ui-bad-ux/dropdown-bad-ui-bad-ux.component.ts
+++ frontend/src/app/random-form/components/question-types/bad-ui-bad-ux/concrete/dropdown-bad-ui-bad-ux/dropdown-bad-ui-bad-ux.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { OptionResponse } from 'src/app/random-form/models/option-response';
 import { QuestionResponse } from 'src/app/random-form/models/question-response';
+import { maxOneOptionChosen } from '../../validators/bad-ui-bad-ux.validation';
 
 @Component({
   selector: 'app-dropdown-bad-ui-bad-ux',
@@ -9,10 +12,28 @@ import { QuestionResponse } from 'src/app/random-form/models/question-response';
 export class DropdownBadUiBadUxComponent implements OnInit {
 
   @Input() question: QuestionResponse;
+  @Input() questionFormGroup: FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.questionFormGroup.get('chosenOptions')?.addValidators([Validators.required, maxOneOptionChosen])
+  }
+
+  get getChosenOptionArray(): FormArray {
+    return this.questionFormGroup.get('chosenOptions') as FormArray
+  }
+
+  getOptionControl(index: number): FormControl {
+    return this.getChosenOptionArray.at(index) as FormControl;
+  }
+
+  onSelect(option: HTMLSelectElement) {
+
+    (this.getChosenOptionArray).clear()
+
+    this.getChosenOptionArray.push(new FormControl(JSON.parse(option.value)))
+
   }
 
 }
