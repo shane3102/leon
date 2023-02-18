8d0c94cbd151b3d355916f0ac111e9f7d1566c5c
shane3102 <bkucharczyk2000@gmail.com>
2023-02-18 17:53:41 +0100

    add displaying correct question component by type

--- frontend/src/app/random-form/components/question-types/good-ui-bad-ux/question-good-ui-bad-ux/question-good-ui-bad-ux.component.ts
+++ frontend/src/app/random-form/components/question-types/good-ui-bad-ux/question-good-ui-bad-ux/question-good-ui-bad-ux.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { QuestionTypes } from 'src/app/models/question-types';
+import { QuestionResponse } from 'src/app/random-form/models/question-response';
 
 @Component({
   selector: 'app-question-good-ui-bad-ux',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionGoodUiBadUxComponent implements OnInit {
 
+  @Input() question: QuestionResponse;
+  @Input() wholeForm: FormGroup
+  @Input() resetFormSubject: Subject<void>;
+  questionFormGroup: FormGroup;
+
+  questionTypes = new QuestionTypes();
+
   constructor() { }
 
   ngOnInit(): void {
+    this.questionFormGroup = new FormGroup({
+      'id': new FormControl(this.question.id),
+      'chosenOptions': new FormArray([]),
+      'answer': new FormControl(),
+      'type': new FormControl(this.question.type),
+      'durationToAnswer': new FormControl(null)//TODO liczenie tego
+    });
+    (this.wholeForm.get('answers') as FormArray).push(this.questionFormGroup)
   }
 
 }
