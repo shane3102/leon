c69c83e17e34fe2ab425d3ca84659aa419695343
shane3102 <bkucharczyk2000@gmail.com>
2023-02-18 16:51:55 +0100

    add long answer validation

--- frontend/src/app/random-form/components/question-types/bad-ui-good-ux/concrete/long-answer-bad-ui-good-ux/long-answer-bad-ui-good-ux.component.ts
+++ frontend/src/app/random-form/components/question-types/bad-ui-good-ux/concrete/long-answer-bad-ui-good-ux/long-answer-bad-ui-good-ux.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup, Validators } from '@angular/forms';
+import { Observable, Subscription } from 'rxjs';
 import { QuestionResponse } from 'src/app/random-form/models/question-response';
 
 @Component({
@@ -11,11 +12,25 @@ export class LongAnswerBadUiGoodUxComponent implements OnInit {
 
   @Input() question: QuestionResponse;
   @Input() questionFormGroup: FormGroup;
+  @Input() triedSubmiting: Observable<void>;
+
+  private triedSubmittingSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit(): void {
     this.questionFormGroup.controls['answer']?.setValidators([Validators.required]);
+    this.triedSubmittingSubscription = this.triedSubmiting.subscribe(() => {
+      this.questionFormGroup.controls['answer'].markAsTouched();
+    })
+  }
+
+  touchedNotFilled(): boolean {
+    return (this.questionFormGroup.controls['answer'] && this.questionFormGroup.controls['answer'].touched && this.questionFormGroup.controls['answer'].errors?.['required'])
+  }
+
+  touchedValid(): boolean {
+    return (this.questionFormGroup.controls['answer'] && this.questionFormGroup.controls['answer'].touched && this.questionFormGroup.controls['answer'].valid)
   }
 
 }
