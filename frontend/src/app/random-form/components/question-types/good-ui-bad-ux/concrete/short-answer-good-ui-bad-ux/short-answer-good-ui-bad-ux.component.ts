d3f0d78fea39fb2f2295cfa35d8aa1f6196d6e24
shane3102 <bkucharczyk2000@gmail.com>
2023-02-21 17:01:03 +0100

    add implementation of short answer question type

--- frontend/src/app/random-form/components/question-types/good-ui-bad-ux/concrete/short-answer-good-ui-bad-ux/short-answer-good-ui-bad-ux.component.ts
+++ frontend/src/app/random-form/components/question-types/good-ui-bad-ux/concrete/short-answer-good-ui-bad-ux/short-answer-good-ui-bad-ux.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable, Subscription } from 'rxjs';
+import { QuestionResponse } from 'src/app/random-form/models/question-response';
 
 @Component({
   selector: 'app-short-answer-good-ui-bad-ux',
   templateUrl: './short-answer-good-ui-bad-ux.component.html',
-  styleUrls: ['./short-answer-good-ui-bad-ux.component.css']
+  styleUrls: ['../../style/good-ui-bad-ux-style.css','./short-answer-good-ui-bad-ux.component.css']
 })
 export class ShortAnswerGoodUiBadUxComponent implements OnInit {
+  
+  @Input() question: QuestionResponse;
+  @Input() questionFormGroup: FormGroup;
+  @Input() resetFormSubject: Observable<void>;
 
+  private resetFormSubscription: Subscription;
+
+  private id: number;
+  private type: string;
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.questionFormGroup.controls['answer']?.setValidators([Validators.required]);
+
+    this.id = this.questionFormGroup.get('id')?.value;
+    this.type = this.questionFormGroup.get('type')?.value;
+
+    this.resetFormSubscription = this.resetFormSubject.subscribe(() => {
+      this.onReset();
+    })
+  }
+
+  onReset() {
+    setTimeout(() => {
+      this.questionFormGroup.setControl('answer', new FormControl(""))
+      this.questionFormGroup.setControl('id', new FormControl(this.id))
+      this.questionFormGroup.setControl('type', new FormControl(this.type))
+    }, 0);
   }
 
 }
