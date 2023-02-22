0535a1ec9b71c8d5fdbae884b8fd241e83df0e6f
shane3102 <bkucharczyk2000@gmail.com>
2023-02-22 16:13:09 +0100

    add displaying correct component by question type

--- frontend/src/app/random-form/components/form-types/random-form-good-ui-good-ux/random-form-good-ui-good-ux.component.ts
+++ frontend/src/app/random-form/components/form-types/random-form-good-ui-good-ux/random-form-good-ui-good-ux.component.ts
@@ -1,4 +1,8 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { Observable, of, Subject } from 'rxjs';
+import { FormToCompleteResponse } from 'src/app/random-form/models/form-to-complete-response';
+import { RandomFormService } from 'src/app/random-form/services/random-form.service';
 
 @Component({
   selector: 'app-random-form-good-ui-good-ux',
@@ -7,11 +11,40 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 })
 export class RandomFormGoodUiGoodUxComponent implements OnInit {
 
+  @Input() formToComplete: FormToCompleteResponse = new FormToCompleteResponse();
+
   @Output() formSentEvent = new EventEmitter();
 
-  constructor() { }
+  randomFormGroup: FormGroup;
+
+  triedSubmitingSubject: Subject<void> = new Subject<void>()
+  triedSubmitting: Observable<boolean> = of(false);
+
+  constructor(private randomFormService: RandomFormService) { }
 
   ngOnInit(): void {
+    this.randomFormGroup = new FormGroup({
+      'answers': new FormArray([]),
+      'uxLevel': new FormControl('GOOD'),
+      'uiLevel': new FormControl('BAD'),
+      'durationToAnswer': new FormControl(null) //TODO liczenie tego 
+    })
   }
 
+  submitForm(request: any) {
+    if (!this.randomFormGroup.invalid) {
+      this.randomFormService.submitRandomForm(request).subscribe({
+        next: res => {
+          this.formSentEvent.emit();
+        }
+      })
+    } else {
+      this.triedSubmitingSubject.next();
+      this.triedSubmitting = of(true);
+    }
+  }
+
+  touchedNotFilled(): boolean {
+    return this.randomFormGroup && this.randomFormGroup.touched && this.randomFormGroup.invalid
+  }
 }
