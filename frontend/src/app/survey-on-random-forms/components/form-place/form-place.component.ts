67654c3e664bafa189bdd8c78e89d2f7232f9c29
shane3102 <bkucharczyk2000@gmail.com>
2023-02-28 21:17:26 +0100

    add validation digits as nackground, add validation message when trying to add two forms in one place

--- frontend/src/app/survey-on-random-forms/components/form-place/form-place.component.ts
+++ frontend/src/app/survey-on-random-forms/components/form-place/form-place.component.ts
@@ -1,5 +1,6 @@
 import { CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { FormUiUxRequest } from 'src/app/models/form-ui-ux-request';
 import { EmmittForm } from '../../model/emitt-form';
 
@@ -11,10 +12,10 @@ import { EmmittForm } from '../../model/emitt-form';
 export class FormPlaceComponent implements OnInit {
 
   @Input() place: number;
-
   @Output() placedOutput: EventEmitter<EmmittForm> = new EventEmitter();
 
   form: FormUiUxRequest[] = [];
+  triedAddingTwoFormsOnOnePlace: Observable<boolean> = of(false);
 
   constructor() { }
 
@@ -24,8 +25,27 @@ export class FormPlaceComponent implements OnInit {
   onDrop(event: CdkDragDrop<FormUiUxRequest[]>) {
     if (this.form.length <= 0) {
       transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex)
+      this.placedOutput.emit({ 'place': this.place, 'form': this.form[0] })
+    } else {
+      this.triedAddingTwoFormsOnOnePlace = of(true);
+      setTimeout(() => {
+        this.triedAddingTwoFormsOnOnePlace = of(false);
+      }, 2000)
     }
-    this.placedOutput.emit({ 'place': this.place, 'form': this.form[0] })
+  }
+
+  getRomanNumber() {
+    switch (this.place) {
+      case 0:
+        return 'I';
+      case 1:
+        return 'II';
+      case 2:
+        return 'III';
+      case 3:
+        return 'IV';
+    }
+    return "IMPLEMENT YOU MORON";
   }
 
 }
