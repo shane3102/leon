09719a8899e604905a16693955c10332c375a24f
shane3102 <bkucharczyk2000@gmail.com>
2023-02-28 19:17:00 +0100

    add draging and placing in general component

--- frontend/src/app/survey-on-random-forms/components/survey-on-random-forms/survey-on-random-forms.component.ts
+++ frontend/src/app/survey-on-random-forms/components/survey-on-random-forms/survey-on-random-forms.component.ts
@@ -1,5 +1,9 @@
+import { CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, Input, OnInit } from '@angular/core';
 import { FormUiUxRequest } from '../../../models/form-ui-ux-request';
+import { EmmittForm } from '../../model/emitt-form';
+import { FormSurveyRequest } from '../../model/form-survey-request';
+import { FormSurveyService } from '../../services/form-survey-service';
 
 @Component({
   selector: 'app-survey-on-random-forms',
@@ -9,12 +13,41 @@ import { FormUiUxRequest } from '../../../models/form-ui-ux-request';
 export class SurveyOnRandomFormsComponent implements OnInit {
 
   @Input() forms: FormUiUxRequest[] = [
-    { uiLevel:'BAD', uxLevel:'BAD', formNumber: 1 }, {uiLevel:'BAD', uxLevel:'GOOD', formNumber: 2}, {uiLevel:'GOOD', uxLevel:'BAD', formNumber: 3}, {uiLevel:'GOOD', uxLevel:'GOOD', formNumber: 4}
+    { uiLevel: 'BAD', uxLevel: 'BAD', formNumber: 1 }, { uiLevel: 'BAD', uxLevel: 'GOOD', formNumber: 2 }, { uiLevel: 'GOOD', uxLevel: 'BAD', formNumber: 3 }, { uiLevel: 'GOOD', uxLevel: 'GOOD', formNumber: 4 }
   ];
 
-  constructor() { }
+  commentOnForms: string = "";
+
+  formsLength: void[];
+
+  responseFormUiUxOrder: Map<number, FormUiUxRequest> = new Map();
+
+  constructor(formSurveyService: FormSurveyService) { }
 
   ngOnInit(): void {
+    this.formsLength = this.forms.map(() => { });
+  }
+
+  onDropBack(event: CdkDragDrop<FormUiUxRequest[]>) {
+    transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex)
+    this.forms.sort((e1, e2) => e1.formNumber - e2.formNumber)
+  }
+
+  onPlaced(emmitedForm: EmmittForm) {
+    this.responseFormUiUxOrder.set(emmitedForm.place, emmitedForm.form);
+    console.log(Array.from(new Map([...this.responseFormUiUxOrder].sort((a, b) => a[0] - b[0]))).map(el => el[1]));
+  }
+
+  submit() {
+
+    let orderOfForms = Array.from(new Map([...this.responseFormUiUxOrder].sort((a, b) => a[0] - b[0]))).map(el => el[1]);
+
+    let formSurveyRequest: FormSurveyRequest = {
+      'formsInOrder': orderOfForms,
+      'commentOnForms': this.commentOnForms
+    }
+
+    console.log(formSurveyRequest);
   }
 
 }
