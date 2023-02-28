a2660e16d8af2f02cf7c95cdb1ef4314534206b3
shane3102 <bkucharczyk2000@gmail.com>
2023-02-28 19:17:21 +0100

    register new components and import modules

--- frontend/src/app/survey-on-random-forms/survey-on-random-forms.module.ts
+++ frontend/src/app/survey-on-random-forms/survey-on-random-forms.module.ts
@@ -2,16 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SurveyOnRandomFormsComponent } from './components/survey-on-random-forms/survey-on-random-forms.component';
 import { FormPanelRepresentationComponent } from './components/form-panel-representation/form-panel-representation.component';
-
-
+import { FormModule } from '../form/form.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DragDropModule } from "@angular/cdk/drag-drop";
+import { FormPlaceComponent } from './components/form-place/form-place.component'
 
 @NgModule({
   declarations: [
     SurveyOnRandomFormsComponent,
-    FormPanelRepresentationComponent
+    FormPanelRepresentationComponent,
+    FormPlaceComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormModule,
+    ReactiveFormsModule,
+    DragDropModule
   ],
   exports:[
     SurveyOnRandomFormsComponent
