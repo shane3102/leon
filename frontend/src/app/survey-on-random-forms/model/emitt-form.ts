a3fa83bdf3d126bc673524f2e8895b2e89431d32
shane3102 <bkucharczyk2000@gmail.com>
2023-02-28 19:13:30 +0100

    add models for emmiting form and submitting form order with comment

---
+++ frontend/src/app/survey-on-random-forms/model/emitt-form.ts
@@ -0,0 +1,6 @@
+import { FormUiUxRequest } from "src/app/models/form-ui-ux-request";
+
+export class EmmittForm {
+    place: number;
+    form: FormUiUxRequest;
+}
---
+++ frontend/src/app/survey-on-random-forms/model/form-survey-request.ts
@@ -0,0 +1,6 @@
+import { FormUiUxRequest } from "src/app/models/form-ui-ux-request";
+
+export class FormSurveyRequest {
+    formsInOrder: FormUiUxRequest[];
+    commentOnForms: string;
+}
