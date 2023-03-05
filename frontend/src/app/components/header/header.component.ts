cd7b584ad44d4020a9169aa2e45c75190a5da33d
shane3102 <bkucharczyk2000@gmail.com>
2023-03-05 17:02:37 +0100

    add home icon

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faHome } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-header',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+
+  faHome = faHome;
 
   constructor() { }
 
