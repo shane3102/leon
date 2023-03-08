2c80e741b0f90675c99f23fd4cca30f1694c7e95
shane3102 <bkucharczyk2000@gmail.com>
2023-03-08 13:05:05 +0100

    implement counting time to single choice question type

--- frontend/src/app/form-random/components/question-types/good-ui-good-ux/concrete/single-choice-good-ui-good-ux/single-choice-good-ui-good-ux.component.ts
+++ frontend/src/app/form-random/components/question-types/good-ui-good-ux/concrete/single-choice-good-ui-good-ux/single-choice-good-ui-good-ux.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Observable, Subscription } from 'rxjs';
+import { FormChangeSubject } from 'src/app/form-random/models/form-change-subject';
+import { FormChanged } from 'src/app/form-random/models/form-changed';
 import { OptionResponse } from 'src/app/form-random/models/option-response';
 import { QuestionResponse } from 'src/app/models/question-response';
 
@@ -11,13 +13,19 @@ import { QuestionResponse } from 'src/app/models/question-response';
 })
 export class SingleChoiceGoodUiGoodUxComponent implements OnInit {
 
+  focus = false;
+
   @Input() question: QuestionResponse;
   @Input() questionFormGroup: FormGroup;
   @Input() triedSubmiting: Observable<void>;
+  @Input() formResultChanged: Observable<FormChangeSubject> = new Observable<FormChangeSubject>();
 
-  focus = false;
+  @Output() formChanged = new EventEmitter<FormChanged>()
 
   private triedSubmittingSubscription: Subscription;
+  private formResultChangedSubscription: Subscription;
+
+  private currentFormResultChange: FormChangeSubject = new FormChangeSubject(undefined, 0, 0, Date.now());
 
   constructor() { }
 
@@ -25,6 +33,10 @@ export class SingleChoiceGoodUiGoodUxComponent implements OnInit {
     this.questionFormGroup.get('chosenOptions')?.addValidators([Validators.required]);
     this.triedSubmittingSubscription = this.triedSubmiting.subscribe(() => {
       this.getChosenOptionArray.markAsTouched();
+    })
+
+    this.formResultChangedSubscription = this.formResultChanged.subscribe(change => {
+      this.currentFormResultChange = change;
     })
   }
 
@@ -41,6 +53,25 @@ export class SingleChoiceGoodUiGoodUxComponent implements OnInit {
       this.getChosenOptionArray.push(new FormControl(value))
     }
 
+    this.formChange();
+
+  }
+
+  formChange() {
+
+    let changeTime: number = Date.now();
+
+    let changeTimeInMilliseconds: number;
+
+    if (this.currentFormResultChange.id == this.question.id) {
+      changeTimeInMilliseconds = changeTime - this.currentFormResultChange.startedFillingForm - this.currentFormResultChange.startedFillingQuestion;
+      this.questionFormGroup.get('durationToAnswerInMilliseconds')?.setValue(changeTimeInMilliseconds)
+    } else {
+      changeTimeInMilliseconds = changeTime - this.currentFormResultChange.startedFillingForm - this.currentFormResultChange.finishedFillingQuestion - this.currentFormResultChange.startedFillingQuestion;
+      this.questionFormGroup.get('durationToAnswerInMilliseconds')?.setValue(changeTimeInMilliseconds + this.questionFormGroup.get('durationToAnswerInMilliseconds')?.value)
+    }
+
+    this.formChanged.emit(new FormChanged(this.question.id, changeTimeInMilliseconds));
   }
 
   touchedNotFilled(): boolean {
