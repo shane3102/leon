296851f95e4d217bbe6ca07be6f416446ca27dd1
shane3102 <bkucharczyk2000@gmail.com>
2023-03-08 11:38:04 +0100

    implement counting time to multiple choice question type

--- frontend/src/app/form-random/components/question-types/good-ui-bad-ux/concrete/multiple-choice-good-ui-bad-ux/multiple-choice-good-ui-bad-ux.component.ts
+++ frontend/src/app/form-random/components/question-types/good-ui-bad-ux/concrete/multiple-choice-good-ui-bad-ux/multiple-choice-good-ui-bad-ux.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatRadioChange } from '@angular/material/radio';
 import { Observable, Subscription } from 'rxjs';
+import { FormChangeSubject } from 'src/app/form-random/models/form-change-subject';
+import { FormChanged } from 'src/app/form-random/models/form-changed';
 import { OptionResponse } from 'src/app/form-random/models/option-response';
 import { QuestionResponse } from 'src/app/models/question-response';
 
@@ -15,8 +17,14 @@ export class MultipleChoiceGoodUiBadUxComponent implements OnInit {
   @Input() question: QuestionResponse;
   @Input() questionFormGroup: FormGroup;
   @Input() resetFormSubject: Observable<void>;
+  @Input() formResultChanged: Observable<FormChangeSubject> = new Observable<FormChangeSubject>();
+
+  @Output() formChanged = new EventEmitter<FormChanged>()
 
   private resetFormSubscription: Subscription;
+  private formResultChangedSubscription: Subscription;
+
+  private currentFormResultChange: FormChangeSubject = new FormChangeSubject(undefined, 0, 0, Date.now());
 
   private id: number;
   private type: string;
@@ -34,6 +42,10 @@ export class MultipleChoiceGoodUiBadUxComponent implements OnInit {
     })
 
     this.question.options.forEach(option => { option.checked = false })
+
+    this.formResultChangedSubscription = this.formResultChanged.subscribe(change => {
+      this.currentFormResultChange = change;
+    })
   }
 
   get getChosenOptionArray(): FormArray {
@@ -53,7 +65,26 @@ export class MultipleChoiceGoodUiBadUxComponent implements OnInit {
       } else {
         this.getChosenOptionArray.removeAt(indexOfCheckedElement);
       }
+
+      this.formChange();
     })
+  }
+
+  formChange() {
+
+    let changeTime: number = Date.now();
+
+    let changeTimeInMilliseconds: number;
+
+    if (this.currentFormResultChange.id == this.question.id) {
+      changeTimeInMilliseconds = changeTime - this.currentFormResultChange.startedFillingForm - this.currentFormResultChange.startedFillingQuestion;
+      this.questionFormGroup.get('durationToAnswerInMilliseconds')?.setValue(changeTimeInMilliseconds)
+    } else {
+      changeTimeInMilliseconds = changeTime - this.currentFormResultChange.startedFillingForm - this.currentFormResultChange.finishedFillingQuestion - this.currentFormResultChange.startedFillingQuestion;
+      this.questionFormGroup.get('durationToAnswerInMilliseconds')?.setValue(changeTimeInMilliseconds + this.questionFormGroup.get('durationToAnswerInMilliseconds')?.value)
+    }
+
+    this.formChanged.emit(new FormChanged(this.question.id, changeTimeInMilliseconds));
   }
 
   onReset() {
