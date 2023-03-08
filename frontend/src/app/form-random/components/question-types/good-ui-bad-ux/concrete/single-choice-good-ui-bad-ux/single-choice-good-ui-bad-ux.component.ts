a2b340cd6c15231f8c88c0037776c2658f37c13d
shane3102 <bkucharczyk2000@gmail.com>
2023-03-08 11:48:08 +0100

    implement counting time to single choice question type

--- frontend/src/app/form-random/components/question-types/good-ui-bad-ux/concrete/single-choice-good-ui-bad-ux/single-choice-good-ui-bad-ux.component.ts
+++ frontend/src/app/form-random/components/question-types/good-ui-bad-ux/concrete/single-choice-good-ui-bad-ux/single-choice-good-ui-bad-ux.component.ts
@@ -1,10 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatCheckboxChange } from '@angular/material/checkbox';
 import { Observable, Subscription } from 'rxjs';
 import { OptionResponse } from 'src/app/form-random/models/option-response';
 import { QuestionResponse } from 'src/app/models/question-response';
 import { maxOneOptionChosen } from 'src/app/form-random/validators/form.validation';
+import { FormChangeSubject } from 'src/app/form-random/models/form-change-subject';
+import { FormChanged } from 'src/app/form-random/models/form-changed';
 
 @Component({
   selector: 'app-single-choice-good-ui-bad-ux',
@@ -16,8 +18,14 @@ export class SingleChoiceGoodUiBadUxComponent implements OnInit {
   @Input() question: QuestionResponse;
   @Input() questionFormGroup: FormGroup;
   @Input() resetFormSubject: Observable<void>;
+  @Input() formResultChanged: Observable<FormChangeSubject> = new Observable<FormChangeSubject>();
+
+  @Output() formChanged = new EventEmitter<FormChanged>()
 
   private resetFormSubscription: Subscription;
+  private formResultChangedSubscription: Subscription;
+
+  private currentFormResultChange: FormChangeSubject = new FormChangeSubject(undefined, 0, 0, Date.now());
 
   private id: number;
   private type: string;
@@ -35,6 +43,10 @@ export class SingleChoiceGoodUiBadUxComponent implements OnInit {
       this.onReset();
     });
     this.question.options.forEach(option => { option.checked = false })
+
+    this.formResultChangedSubscription = this.formResultChanged.subscribe(change => {
+      this.currentFormResultChange = change;
+    })
   }
 
   get getChosenOptionArray(): FormArray {
@@ -50,6 +62,25 @@ export class SingleChoiceGoodUiBadUxComponent implements OnInit {
       this.getChosenOptionArray.removeAt(indexOfCheckedElement);
     }
 
+    this.formChange();
+
+  }
+
+  formChange() {
+
+    let changeTime: number = Date.now();
+
+    let changeTimeInMilliseconds: number;
+
+    if (this.currentFormResultChange.id == this.question.id) {
+      changeTimeInMilliseconds = changeTime - this.currentFormResultChange.startedFillingForm - this.currentFormResultChange.startedFillingQuestion;
+      this.questionFormGroup.get('durationToAnswerInMilliseconds')?.setValue(changeTimeInMilliseconds)
+    } else {
+      changeTimeInMilliseconds = changeTime - this.currentFormResultChange.startedFillingForm - this.currentFormResultChange.finishedFillingQuestion - this.currentFormResultChange.startedFillingQuestion;
+      this.questionFormGroup.get('durationToAnswerInMilliseconds')?.setValue(changeTimeInMilliseconds + this.questionFormGroup.get('durationToAnswerInMilliseconds')?.value)
+    }
+
+    this.formChanged.emit(new FormChanged(this.question.id, changeTimeInMilliseconds));
   }
 
   onReset() {
