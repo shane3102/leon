4c0361621ca6a0793941929b2a95bd4fe2df189b
shane3102 <bkucharczyk2000@gmail.com>
2023-03-10 19:33:52 +0100

    add routing to new component

---
+++ frontend/src/app/form-random-statistics/components/form-random-statistics/form-random-statistics.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-random-statistics',
+  templateUrl: './form-random-statistics.component.html',
+  styleUrls: ['./form-random-statistics.component.css']
+})
+export class FormRandomStatisticsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
