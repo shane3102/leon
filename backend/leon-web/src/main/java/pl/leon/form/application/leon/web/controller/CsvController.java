02eb94a37ba81035e5e1b01f35fe970e0473241e
shane3102 <bkucharczyk2000@gmail.com>
2023-03-12 20:03:32 +0100

    add new endpoint for new methods

--- backend/leon-web/src/main/java/pl/leon/form/application/leon/web/controller/CsvController.java
+++ backend/leon-web/src/main/java/pl/leon/form/application/leon/web/controller/CsvController.java
@@ -3,6 +3,7 @@ package pl.leon.form.application.leon.web.controller;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.leon.form.application.leon.export.CsvExporter;
@@ -28,6 +29,14 @@ public class CsvController {
         log.info("getAnsweredQuestionsCsvReport()");
         byte[] response = csvExporter.csvReportQuestionsAnswered();
         log.info("getAnsweredQuestionsCsvReport() = success");
+        return response;
+    }
+
+    @GetMapping("/completed-form-results/{id}")
+    public byte[] getCompletedFormResultsById(@PathVariable Long id) {
+        log.info("getCompletedFormResultsById({})", id);
+        byte[] response = csvExporter.csvReportFormCompletedResults(id);
+        log.info("getCompletedFormResultsById({}) = success", id);
         return response;
     }
 }
